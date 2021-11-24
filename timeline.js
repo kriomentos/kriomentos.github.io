67990c694059bbc689096ff1310b49cfd4099283
kriomentos <sznajder.konrad@gmail.com>
2021-11-24 21:08:36 +0100

    Add files via upload

---
+++ timeline.js
@@ -0,0 +1,13 @@
+export const data=[
+  {id: 1, title: 1, text: 'Sample text', src: './assets/favicon.png', altext: 'This is a logo image'},
+  {id: 2, title: 2, text: 'Another sample', src: './assets/fwog.png'},
+  {id: 3, title: 3, text: 'Third example sample', src: './assets/Uncle.png'},
+  {id: 4, title: 'Awesome title', text: ''},
+  {id: 5, title: 'Ooops almost broke something', text: 'Member to escape your specials kids!'},
+  {id: 6, title: 'Quick fix is the best fix\\', text: 'Don\'t overcomplicate'},
+  {id: 7, title: 'Do do doo do', text: 'Dancing with the DOM'},
+  {id: 8, title: 'Title', text: 'Sample text'},
+  {id: 9, title: 'Title', text: 'Sample text'},
+  {id: 10, title: 'Lorem ipsum dolor sit amet', text: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean odio eros, vulputate sit amet convallis vehicula, vehicula sit amet lorem.'},
+  {id: 11, title: 'Title', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis velit nunc, et accumsan turpis venenatis eu. Curabitur fringilla nisi vel laoreet elementum. Nulla facilisi. Suspendisse consequat viverra urna ac auctor. Curabitur dapibus massa ut rutrum condimentum. Praesent sodales dui nec felis porta elementum ac vitae magna. Praesent molestie elit et tincidunt pharetra.'}
+]
